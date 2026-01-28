b0fa6f10b03e6705dc7876e9aa71b65540889f1b
SylvanDSouzaUni <30258858@blackpool.ac.uk>
2026-01-28 09:17:10 +0000

    Update main.py
    Updated main.py for Version 1.0

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world!")
+from database import create_schema, get_connection
+from authentication import create_starting_admin, login, create_user
+
+
+def main_loop():
+
+    create_schema()
+    create_starting_admin()
+
+    while True:
+        print("[CONSOLE] PLEASE LOG IN WITH YOUR CREDENTIALS")
+        user = None
+        while not user:
+            user = login()
+
+if __name__ == "__main__":
+    main_loop()
